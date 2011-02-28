d2604416bd10961be04dc49a6757cbff4749beac
qmp <glang@lavabit.com>
2011-02-28 14:17:28 +0100

    Remove transparency for unfocused windows

--- .config/awesome/rules-signals.lua
+++ .config/awesome/rules-signals.lua
@@ -45,6 +45,3 @@ client.add_signal("manage", function (c, startup)
         end
     end
 end)
-
-client.add_signal("focus", function(c) c.border_color = beautiful.border_focus c.opacity = 1 end)
-client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal c.opacity = 0.95 end)
