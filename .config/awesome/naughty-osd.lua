7338a7d3781f8ca0c03a2bc5a7dce4f1e0c8aabb
qmp <glang@lavabit.com>
2011-05-19 17:17:10 +0200

    naughty configuration

---
+++ .config/awesome/naughty-osd.lua
@@ -0,0 +1,16 @@
+naughty.config.default_preset.timeout          = 5
+naughty.config.default_preset.screen           = 1
+naughty.config.default_preset.position         = "top_right"
+naughty.config.default_preset.margin           = 4
+naughty.config.default_preset.height           = 16
+naughty.config.default_preset.width            = 300
+naughty.config.default_preset.gap              = 1
+naughty.config.default_preset.ontop            = true
+naughty.config.default_preset.font             = beautiful.font or "Verdana 8"
+naughty.config.default_preset.icon             = nil
+naughty.config.default_preset.icon_size        = 16
+naughty.config.default_preset.fg               = beautiful.fg_focus or '#ffffff'
+naughty.config.default_preset.bg               = beautiful.bg_focus or '#535d6c'
+naughty.config.presets.normal.border_color     = beautiful.border_focus or '#535d6c'
+naughty.config.default_preset.border_width     = 1
+naughty.config.default_preset.hover_timeout    = nil
