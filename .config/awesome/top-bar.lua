1d138a1bee574bd01e0941d7f969a247ef98a35a
qmp <glang@lavabit.com>
2011-07-28 21:42:41 +0200

    Assume all widgets are of the type textbox in bashets

--- .config/awesome/top-bar.lua
+++ .config/awesome/top-bar.lua
@@ -4,7 +4,7 @@ vicious.register(batwidget, vicious.widgets.bat, "$1$2%", 61, "BAT0")
 mytextclock = awful.widget.textclock({ align = "right" }, " %a %b %d, %H:%M ", 1)
 
 mytaskw = widget({type = "textbox", popup = nil, name="mytaskw", separator='|' })
-bashets.register('task_summary.sh', {widget = mytaskw, format = '<b><span color="red">$1</span>/<span color="cyan">$2</span>/<span color="white">$3</span></b>"', async = true, update_time = 120 })
+bashets.register('task_summary.sh', {widget = mytaskw, format = '<b><span color="red">$1</span>/<span color="cyan">$2</span>/<span color="white">$3</span></b>"', async = true, update_time = 60})
 mytaskw_popup = nil
 function mytaskw_get_popup_text ()
 	awful.util.spawn('/home/qmp/.config/awesome/scripts/task_cache.sh')
