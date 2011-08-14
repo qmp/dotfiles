b3cffba0434a782b9aee6b0de4866cb35aa58b16
qmp <glang@lavabit.com>
2011-08-14 15:41:49 +0200

    Fix colors

--- .config/awesome/scripts/task_cache.sh
+++ .config/awesome/scripts/task_cache.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 function htmlify() {
   echo "<span color='red'>`/home/qmp/.config/awesome/scripts/task_reader.sh high`</span>"
-  echo "<span color='cyan'>`/home/qmp/.config/awesome/scripts/task_reader.sh normal`"
-  echo "</span><span color='white'>`/home/qmp/.config/awesome/scripts/task_reader.sh low`</span>"
+  echo "<span color='black'>`/home/qmp/.config/awesome/scripts/task_reader.sh normal`"
+  echo "</span><span color='cyan'>`/home/qmp/.config/awesome/scripts/task_reader.sh low`</span>"
 
 }
 
