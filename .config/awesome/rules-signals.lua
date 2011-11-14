ebdbeeceffd174ab6a58e7a50f25788dbccfe4a8
qmp <glang@lavabit.com>
2011-11-14 19:49:52 +0100

    Rule for psi

--- .config/awesome/rules-signals.lua
+++ .config/awesome/rules-signals.lua
@@ -31,6 +31,8 @@ awful.rules.rules = {
       properties = { tag = tags[1][7] } },
     { rule = { class = "Pidgin" },
       properties = { tag = tags[1][7] } },
+    { rule = { class = "psi" },
+      properties = { tag = tags[1][7] } },
     { rule = { class = "Hotot" },
       properties = { tag = tags[1][7] } },
     { rule = { class = "Transmission-gtk" },
